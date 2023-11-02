05346b9e5fc3ecc752212da714af63cdc0d52005
Fikri <140414207+fikrifrmn@users.noreply.github.com>
2023-11-02 12:53:22 +0700

    Add files via upload

---
+++ fikri/src/fikri/umsu.java
@@ -0,0 +1,14 @@
+
+package fikri;
+
+public class umsu {
+
+    public static void main(String[] args) {
+        int nilai1,nilai2;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+" FIKTI UMSU");
+        }
+    }
+    
+}
+
